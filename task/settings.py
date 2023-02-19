9d3af3e97f60eea27f483cdb30572a70a54c8a8e
nithindaniel1 <nithindaniel2018@gmail.com>
2023-02-19 18:13:50 +0530

    migrate allowed host error fixed

--- task/settings.py
+++ task/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aoi',
+    'api',
 
     'rest_framework',
 ]
