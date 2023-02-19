b4fb58c3019ffa9ec5d318765c574d4c75238147
nithindaniel1 <nithindaniel2018@gmail.com>
2023-02-19 19:09:03 +0530

    initial validation successfull

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Student
 
 # Create your views here.
+class ValidStudent(APIView):
+    def post(self,request):
+        name = request.data.get('name')
+        # print(name)
+        name_validate = Student.objects.filter(name=name)
+        if name_validate.exists():
+            print("It is there")
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
