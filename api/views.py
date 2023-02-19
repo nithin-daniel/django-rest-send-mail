30e14ffd56f1ce71a70858754a06a42934a4bb41
nithindaniel1 <nithindaniel2018@gmail.com>
2023-02-19 21:19:09 +0530

    validation issues fixed

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from .models import Student
 
@@ -9,9 +10,22 @@ class ValidStudent(APIView):
     def post(self,request):
         name = request.data.get('name')
         # print(name)
-        name_validate = Student.objects.filter(name=name)
-        if name_validate.exists():
-            print("It is there")
+        # name_validate = Student.objects.get(name=name)
+        # email = name_validate.email
+        # validation = Student.objects.get(name=name)
+        # if  validation==True:
+        #     # name_validate = Student.objects.only('id').get(name=name).id
+        #     print("It is there")
+        #     # print(name_validate.email)
+        #     return Response(status=status.HTTP_200_OK)
+        # else:
+        #     print("Not there")
+        #     return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            validation = Student.objects.get(name=name)
+            print(validation)
             return Response(status=status.HTTP_200_OK)
-        else:
+        except ObjectDoesNotExist:
+            print('not')
             return Response(status=status.HTTP_404_NOT_FOUND)
